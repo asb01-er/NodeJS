5a943898f7b57149bf0ae81411db53baf42360f3
asb01-er <ekelemernesto17@gmail.com>
2025-10-13 15:49:24 +0200

    new codes

--- Unit0/lesson1/lesson1.js
+++ Unit0/lesson1/lesson1.js
@@ -2,7 +2,6 @@ class Goat {
 eat(foodType){
 console.log(`I love eating ${foodType}`);
 }}
-billy.eat("tin cans");
 
 let billy = new Goat();
 billy.eat("tin cans");
---
+++ Unit1/lesson6/server_html/main.js
@@ -0,0 +1,105 @@
+const port = 3000;
+const http = require("http");
+const httpStatus = require("http-status-codes");
+const fs = require("fs");
+
+// ex1
+// const routeMap = {
+//     "/":"views/index.html"
+// };
+
+// ex2
+// const getViewUrl = (url) => {
+// return `views${url}.html`;
+// };
+
+// example1
+
+// http.createServer((req, res) => {
+//     res.writeHead(httpStatus.OK, {
+//         "Content_type" : "text/html"
+//     });
+
+//     if (routeMap[req.url]) {
+//         fs.readFile(routeMap[req.url], (error, data) => {
+//             res.write(data);
+//             res.end();
+//         });
+//     } else {
+//         res.end("<h1>Sorry, not found.</h1>");
+//     }
+// })
+
+// example2
+
+// http.createServer((req, res) => {
+//     let viewUrl = getViewUrl(req.url);
+//     fs.readFile(viewUrl, (error, data) => {
+//         if (error) {
+//             res.writeHead(httpStatus.NOT_FOUND);
+//             res.write("<h1>FILE NOT FOUND</h1>");
+//         } else {
+//             res.writeHead(httpStatus.OK, {
+//                 "Content-Type" : "text/html"
+//             });
+//             res.write(data);
+//         }
+//         res.end();
+//     })
+// })
+
+
+
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
+};
+
+http.createServer((req, res) => {
+    let url = req.url;
+    if (url.indexOf("html") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(`./views${url}`, res);
+    } else if (url.indexOf(".js") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/javascript"
+        });
+        customReadFile(`./public/js${url}`, res);
+    } else if (url.indexOf(".css") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "text/css"
+        });
+        customReadFile(`./public/css${url}`, res);
+    } else if (url.indexOf("png") !== -1) {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type": "image/png"
+        });
+        customReadFile(`./public/image${url}`, res);
+    } else {
+        sendErrorResponse(res);
+    }
+})
+
+    .listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
+
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)) {
+        fs.readFile(file_path, (error, data) => {
+            if (error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    } else {
+        sendErrorResponse(res);
+    }
+};
