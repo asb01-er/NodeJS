bd5df2ea72cb7fc844fd9f010cfae6cbb3233582
asb01-er <ekelemernesto17@gmail.com>
2025-10-23 07:03:47 +0200

    new codes

--- Unit2/lesson12/confetti_cuisine/controller/homeController.js
+++ Unit2/lesson12/confetti_cuisine/controller/homeController.js
@@ -1,14 +1,28 @@
-exports.showCourses = (req,res) => {
-    res.render("courses");
-};
-exports.showSignUp = (req, res) => {
-    res.render("contacts");
-};
-exports.postedSignUpForm = (req,res) => {
-    res.render("thanks");
-};
+"use strict";
+var courses = [
+    {
+        title: "Event Driven Cakes",
+        cost: 50
+    },
+    {
+        title: "Asynchronous Artichoke",
+        cost: 25
+    },
+    {
+        title: "Object Oriented Orange Juice",
+        cost: 10
+    }
+];
+
 exports.showCourses = (req, res) => {
     res.render("courses", {
         offeredCourses: courses
     });
+};
+
+exports.showSignUp = (req, res) => {
+    res.render("contact");
+};
+exports.postedSignUpForm = (req,res) => {
+    res.render("thanks");
 };
--- Unit2/lesson12/confetti_cuisine/main.js
+++ Unit2/lesson12/confetti_cuisine/main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const layouts = require("express-ejs-layouts")
+const layouts = require("express-ejs-layouts");
+const errorController = require("./controller/errorController");
+const homeController = require("./controller/homeController");
 
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 app.use(layouts);
 
 app.use(express.urlencoded({
@@ -14,13 +16,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3000);
+
 app.get("/", (req, res) => {
-    res.send("Welcome to Confetti Cuisine!");
+    res.render("index");
 });
 
 app.get("/courses", homeController.showCourses);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 app.listen(app.get("port"), () => {
     console.log(
